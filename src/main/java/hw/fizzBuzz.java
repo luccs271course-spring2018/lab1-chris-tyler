34655c29b5163a1b47de0b9ba3df6ca9760a2a32
tnguyen28 <tnguyen28@luc.edu>
2017-09-01 10:17:57 -0500

    initial import

---
+++ src/main/java/hw/fizzBuzz.java
@@ -0,0 +1,23 @@
+package hw;
+
+public class fizzBuzz {
+
+    public static void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            boolean fb = false;
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+
+            } else {
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
