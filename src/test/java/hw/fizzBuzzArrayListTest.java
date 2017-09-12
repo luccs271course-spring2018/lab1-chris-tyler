dad70ee824258aede80d760d869ce7db81803819
Tyler Nguyen <tnguyen28@luc.edu>
2017-09-12 02:10:55 +0000

    update

--- src/test/java/hw/fizzBuzzArrayListTest.java
+++ src/test/java/hw/fizzBuzzArrayListTest.java
@@ -8,34 +8,49 @@ import static org.junit.Assert.*;
 
 public class fizzBuzzArrayListTest {
 
+    private fizzBuzzList fixture;
+
+    @Before
+    public void setUp() throws Exception {
+        fixture = new fizzBuzzList();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        fixture = null;
+    }
+
     //Input cannot be negative or zero.
     @Test
     public void testNZ() {
-    	//Either of these inputs would return null so this seemed more efficient.
-        assertNull(fizzBuzzList.FizzBuzz4(-10));
-        assertNull(fizzBuzzList.FizzBuzz4(-1));
-        assertNull(fizzBuzzList.FizzBuzz4(0));
+        assertNull(fixture.FizzBuzz4(-10));
+        assertNull(fixture.FizzBuzz4(-1));
+        assertNull(fixture.FizzBuzz4(0));
+
     }
 
     @Test
     public void testP1() {
+        assertNotNull(fixture);
         //If the input is one, fizzbuzz should only run once.
         List<String> s = Arrays.asList("1");
-        assertEquals(s, fizzBuzzList.FizzBuzz4(1));
+        assertEquals(s, fixture.FizzBuzz4(1));
     }
 
     @Test
     public void testP2() {
+        assertNotNull(fixture);
         //If the input is 7, fizzbuzz should only run 7 times.
         List<String> s = Arrays.asList("1", "2", "fizz", "4", "buzz", "6", "7");
-        assertEquals(s, fizzBuzzList.FizzBuzz4(7));
+        assertEquals(s, fixture.FizzBuzz4(7));
     }
 
     @Test
     public void testP3() {
+        assertNotNull(fixture);
         //If the input is 17, fizzbuzz should only run 17 times.
         List<String> s = Arrays.asList("1", "2", "fizz", "4", "buzz", "6", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz", "16", "17");
-        assertEquals(s, fizzBuzzList.FizzBuzz4(17));
+        assertEquals(s, fixture.FizzBuzz4(17));
     }
 
 
--- src/test/java/hw/fizzBuzzArrayTest.java
+++ src/test/java/hw/fizzBuzzArrayTest.java
@@ -1,46 +1,61 @@
 package hw;
 
-import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class fizzBuzzArrayTest {
 
+    private fizzBuzzArray fixture;
+
+    @Before
+    public void setUp() throws Exception {
+        fixture = new fizzBuzzArray();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        fixture = null;
+    }
     //Input cannot be negative.
     @Test
     public void testN1() {
-        assertNull(fizzBuzzArray.FizzBuzz3(-10));
+        assertNull(fixture.FizzBuzz3(-10));
     }
 
     @Test
     public void testN2() {
-        assertNull(fizzBuzzArray.FizzBuzz3(-1));
+        assertNull(fixture.FizzBuzz3(-1));
     }
 
 
     //Zero is not negative nor positive.
     @Test
     public void testZero() {
-        assertNull(fizzBuzzArray.FizzBuzz3(0));
+        assertNull(fixture.FizzBuzz3(0));
     }
 
     //Making sure the concept of fizz buzz actually works and matches what it should be doing.
     @Test
     public void testP1() {
+        assertNotNull(fixture);
         String[] s = {"1"};
-        assertArrayEquals(s, fizzBuzzArray.FizzBuzz3(1));
+        assertArrayEquals(s, fixture.FizzBuzz3(1));
     }
 
     @Test
     public void testP2() {
+        assertNotNull(fixture);
         String[] s = {"1", "2", "fizz", "4", "buzz", "6", "7"};
-        assertArrayEquals(s, fizzBuzzArray.FizzBuzz3(7));
+        assertArrayEquals(s, fixture.FizzBuzz3(7));
     }
 
     @Test
     public void testP3() {
+        assertNotNull(fixture);
         String[] s = {"1", "2", "fizz", "4", "buzz", "6", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz", "16", "17"};
-        assertArrayEquals(s, fizzBuzzArray.FizzBuzz3(17));
+        assertArrayEquals(s, fixture.FizzBuzz3(17));
     }
 
 
